20fb8538e015968ab1581366d437d8ed5f402062
DanyGuerra <luis_daniel.grra15@hotmail.com>
2025-08-22 21:50:15 -0600

    feat: :sparkles: Response interceptor

---
+++ src/interceptors/response/response.interceptor.ts
@@ -0,0 +1,40 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class ResponseInterceptor<T> implements NestInterceptor<T, any> {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data) => {
+        const ctx = context.switchToHttp();
+        const response = ctx.getResponse();
+        const statusCode = response.statusCode;
+
+        if (data && typeof data === 'object' && 'message' in data) {
+          const responseBody: any = {
+            message: data.message,
+            statusCode,
+          };
+
+          if ('data' in data) {
+            responseBody.data = data.data;
+          }
+
+          return responseBody;
+        }
+
+        return {
+          message: 'success',
+          data,
+          statusCode,
+        };
+      }),
+    );
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import * as cookieParser from 'cookie-parser';
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { ResponseInterceptor } from './interceptors/response/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(cookieParser());
+  app.useGlobalInterceptors(new ResponseInterceptor());
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,

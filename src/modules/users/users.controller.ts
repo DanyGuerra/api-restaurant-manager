2af8a13ebd6bdd5b16a03782af8ffd922b98f074
DanyGuerra <luis_daniel.grra15@hotmail.com>
2025-09-10 21:29:38 -0600

    feat: :sparkles: Get user info by Jwt token

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, Req, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { instanceToPlain } from 'class-transformer';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { Request } from 'express';
+
+interface RequestWithUser extends Request {
+  user: { sub: string; [key: string]: any };
+}
 
 @Controller('users')
 @UseGuards(JwtAuthGuard)
 export class UsersController {
   constructor(private userService: UsersService) {}
+
+  @Get('me')
+  async getMyUser(@Req() req: RequestWithUser) {
+    const userId = req.user['sub'];
+    const user = await this.userService.findById(userId);
+    return instanceToPlain(user);
+  }
 
   @Get(':id')
   async getUserById(@Param('id') id: string) {
